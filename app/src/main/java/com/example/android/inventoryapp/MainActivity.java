9f26a26bcadf217c9cb731615d4925d33942b663
David Burgos Prieto <david.burgos.prieto@gmail.com>
2017-07-17 19:53:40 +0200

    Empty view and FAB.

--- app/src/main/java/com/example/android/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.android.inventoryapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Annotate fields with @BindView and views ID for Butter Knife to find and automatically cast
+    // the corresponding views.
+    @BindView(R.id.main_floating_action_button)
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
