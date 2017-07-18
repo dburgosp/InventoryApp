53d8e709853f0ce024f399e1410f8913c608507b
David Burgos <david.burgos.prieto@gmail.com>
2017-07-18 14:16:50 +0200

    Menu items.

--- app/src/main/java/com/example/android/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/MainActivity.java
@@ -1,23 +1,33 @@
 package com.example.android.inventoryapp;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
     // Annotate fields with @BindView and views ID for Butter Knife to find and automatically cast
     // the corresponding views.
-    @BindView(R.id.main_floating_action_button)
-    FloatingActionButton fab;
+    //@BindView(R.id.main_floating_action_button) FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+        //ButterKnife.bind(this);
+    }
+
+    /**
+     * Specify the options menu for this activity.
+     *
+     * @param menu is the options menu in which the items are placed.
+     * @return true for the menu to be displayed; if we return false it will not be shown.
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
